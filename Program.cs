09ba65e13f7232605dc5b18d83dbea7c30ada06d
Parell <parelldev01@gmail.com>
2022-05-21 17:53:21 -0500

    Clean up

--- Program.cs
+++ Program.cs
@@ -16,16 +16,15 @@ internal class Program
         float Ae = (float)(Math.PI * Math.Pow(De, 2) / 4);
         float At = Ae / Aratio;
 
-        float p0 = 10000000f;
+        float p0 = 50000000f;
         float pe = 101325f;
         float pa = 101325f;
 
         float R = Ru / Mw;
         float ρ0 = p0 / (R * T0); //kg/m3
 
-        var engineMass = 25; // kg
+        var engineMass = 250; // kg
 
-        float Γ;
         float mdot;
         float ve;
         float veq;
@@ -33,11 +32,9 @@ internal class Program
         float thrust;
         float twr;
 
-
         void Run()
         {
-            Γ = (float)Math.Sqrt(k * Math.Pow((1 + k) / 2, (1 + k) / (1 - k)));
-            mdot = (float)(At * p0 / Math.Sqrt(R * T0) * Γ);
+            mdot = (float)(At * p0 / Math.Sqrt(R * T0) * Math.Sqrt(k * Math.Pow((1 + k) / 2, (1 + k) / (1 - k))));
 
             ve = (float)Math.Sqrt(R * T0 * (2 * k / (k - 1)) * (1 - Math.Pow(pe / p0, (k - 1) / k)));
 
@@ -50,20 +47,9 @@ internal class Program
 
         Run();
 
-        var Q = 135350190;
-
-        T0 = Q / (14300 * mdot);
-
-        var Qlose = ((Q / 0.8f) / 0.77f) / 0.83f;
-        var Qin = Qlose + Q;
-
         string Output()
         {
             string text = string.Format("\n" +
-                                     //"Energy ----------------------- \n\n" +
-                                     //"Input            {12} MW \n" +
-                                     //"Lose             {11} MW \n" +
-                                     //"Final            {10} MW \n\n" +
                                      "Temp             {9} K \n" +
                                      "Thrust           {1} kN \n" +
                                      "Mass flow rate   {2} kg/s \n" +
@@ -74,7 +60,7 @@ internal class Program
                                      "Isp              {5} s \n\n" +
                                      "Exit diameter    {7} cm \n" +
                                      "Throat diameter  {8} cm \n", twr, thrust / 1000, mdot, ve, veq, isp, p0 / 1000000,
-                                     (float)Math.Sqrt((Ae / Math.PI) * 4) * 100, (float)Math.Sqrt((At / Math.PI) * 4) * 100, T0, Q / 1000000, Qlose / 1000000, Qin / 1000000);
+                                     (float)Math.Sqrt((Ae / Math.PI) * 4) * 100, (float)Math.Sqrt((At / Math.PI) * 4) * 100, T0);
             return text;
         }
 
